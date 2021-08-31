f2881f102bed2ee2c3903d91126e18fc8e2f8a8d
truongcaolop3 <truongcaolop3@gmail.com>
2021-08-31 20:18:16 +0700

    init

---
+++ src/learn/java/LearnJava.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learn.java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DELL
+ */
+public class LearnJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Sum = " + (a + b));
+    }
+    
+}
